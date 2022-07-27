0eb5da5839321cc45d77fc6620fcba9dd5e19cfa
westkite1201 <westkite1201@gmail.com>
2022-07-27 18:28:01 +0900

    refactor : 소셜 로그인 auth refactor

--- routes/api/authController.js
+++ routes/api/authController.js
@@ -3,18 +3,17 @@ let router = express.Router();
 const jwt = require('../../lib/jwt-util');
 const redisClient = require('../../lib/redis');
 const refresh = require('../../lib/refresh');
-const token = require('../../lib/token');
 const _ = require('lodash');
-let bcrypt = require('bcrypt-nodejs');
 const authMiddleware = require('../../middlewares/auth');
 const helper = require('../../lib/helpers');
-const request = require('request-promise-native');
 const winston = require('winston');
 const logger = winston.createLogger();
 const qs = require('qs');
 const fetch = require('node-fetch');
 const authDaoNew = require('../../model/mysql/authDaoNew');
 const authJwt = require('../../middlewares/auth');
+const resMessage = require('../../lib/resMessage');
+const statusCode = require('../../lib/statusCode');
 
 class Kakao {
   constructor(code) {
@@ -57,16 +56,7 @@ class Naver {
     this.userInfoMethod = 'get';
   }
 }
-// /    GET /api/auth/check
-// const check = (req, res) => {
-//   console.log('check in');
-//   res.json({
-//     success: true,
-//     info: req.decoded
-//   });
-// };
-// router.use('/check', authMiddleware);
-// router.get('/check', check);
+
 bcryptCheck = async (password, rows) => {
   try {
     const jwtToken = await helper.bcryptCompare(password, rows);
@@ -119,6 +109,55 @@ const getUserInfo = async (method, url, access_token) => {
   }
 };
 
+async function signUp({ id, email, social, isExist }) {
+  let jwtToken = null;
+  let refreshToken = null;
+  if (!isExist) {
+    console.log('가입되지 않은 회원입니다.');
+    //회원 가입후 토큰 발급
+    const userData = {
+      mem_username: '',
+      mem_userid: id,
+      mem_email: email,
+      mem_password: '',
+      mem_social: social,
+    };
+    const signUpResponse = await authDaoNew.setMemberSignUp(userData);
+
+    if (!signUpResponse) return 'error';
+    if (signUpResponse) {
+      jwtToken = jwt.sign({
+        id: id,
+        email: email,
+      });
+      refreshToken = jwt.refresh();
+    }
+  } else {
+    console.log('가입된  회원입니다.');
+    jwtToken = jwt.sign({
+      id: id,
+      email: email,
+    });
+    refreshToken = jwt.refresh();
+  }
+  console.log('accessToken =', jwtToken);
+  console.log('refreshToken =', refreshToken);
+  return { jwtToken, refreshToken };
+}
+
+function returnResponse({ res, jwtToken, refreshToken }) {
+  res.cookie('refresh-token', refreshToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 1,
+    httpOnly: true,
+  });
+
+  let responseData = {
+    code: statusCode.OK,
+    message: resMessage.SIGN_IN_SUCCESS,
+    accessToken: jwtToken,
+  };
+  return res.json(responseData);
+}
 /* 소셜 로그인시 
  1. 소셜 로그인시 가입이 되어있는지 확인
  1-1. 가입이 되어있다면 가입 이메일로 로그인
@@ -128,9 +167,7 @@ router.get('/callback/:coperation', async (req, res) => {
   try {
     let coperation = req.params.coperation;
     let authorization_code = req.query.code;
-
     let options;
-
     switch (coperation) {
       case 'google':
         options = new Google(authorization_code);
@@ -155,6 +192,7 @@ router.get('/callback/:coperation', async (req, res) => {
     console.log('options= ', options);
     const tokenInfo = await getAccessToken(options);
     console.log('token = ', tokenInfo);
+
     if (!tokenInfo) return;
     if (coperation === 'google')
       options.userInfoUrl = `https://www.googleapis.com/oauth2/v2/userinfo?access_token=${tokenInfo.access_token}`;
@@ -164,7 +202,6 @@ router.get('/callback/:coperation', async (req, res) => {
       options.userInfoUrl,
       tokenInfo.access_token,
     );
-
     console.log('userInfo = ', userInfo);
 
     //로그인 되었 을 경우
@@ -173,132 +210,108 @@ router.get('/callback/:coperation', async (req, res) => {
         console.log('userInfo ', userInfo);
         const { resultcode, message, response } = userInfo;
         if (resultcode === '00' && message === 'success') {
-          const jwtToken = jwt.sign({
-            id: id,
-            email: email,
-          });
-
-          let responseData = {
-            success: true,
-            userInfo,
-            jwt: jwtToken,
-          };
-          return res.status(200).json(responseData);
+          //1. 회원가입 여부 확인
+          const memberRow = await authDaoNew.getEmailIsAlreadyExist(email);
+          const { EXISTFLAG } = memberRow[0];
+          console.log('member log', memberRow);
+          let jwtToken = null;
+          let refreshToken = null;
+          //회원 가입 안되어있을시
+          if (EXISTFLAG !== 'EXIST') {
+            let { jwtToken: access, refreshToken: refresh } = await signUp({
+              id,
+              email,
+              social: 'naver',
+              isExist: false,
+            });
+            jwtToken = access;
+            refreshToken = refresh;
+          } else if (EXISTFLAG === 'EXIST') {
+            let { jwtToken: access, refreshToken: refresh } = await signUp({
+              id,
+              email,
+              social: 'naver',
+              isExist: true,
+            });
+            jwtToken = access;
+            refreshToken = refresh;
+          }
+          // 발급한 refresh token을 redis에 key를 user의 id로 하여 저장합니다.
+          //await redisClient.set(id, refreshToken);
+          return returnResponse({ res, jwtToken, refreshToken });
         }
       }
 
       if (coperation === 'kakao') {
         const { kakao_account, id } = userInfo;
         if (id && kakao_account) {
-          const jwtToken = token.generateToken(kakao_account);
-
-          let responseData = {
-            success: true,
-            userInfo,
-            jwt: jwtToken,
-          };
-          return res.status(200).json(responseData);
-        }
-      }
-      if (coperation === 'google') {
-        const { id, email, verified_email, picture } = userInfo;
-
-        if (id && email) {
           //1. 회원가입 여부 확인
           const memberRow = await authDaoNew.getEmailIsAlreadyExist(email);
+          const { EXISTFLAG } = memberRow[0];
           console.log('member log', memberRow);
           let jwtToken = null;
           let refreshToken = null;
           //회원 가입 안되어있을시
-          if (memberRow[0].EXISTFLAG !== 'EXIST') {
-            console.log('가입되지 않은 회원입니다.');
-            //회원 가입후 토큰 발급
-            const userData = {
-              mem_username: '',
-              mem_userid: id,
-              mem_email: email,
-              mem_password: '',
-              mem_social: 'google',
-            };
-            console.log(' userData ', userData);
-            const signUpResponse = await authDaoNew.setMemberSignUp(userData);
-            if (!signUpResponse) return 'error';
-            if (signUpResponse) {
-              jwtToken = jwt.sign({
-                id: id,
-                email: email,
-              });
-              refreshToken = jwt.refresh();
-            }
-          } else if (memberRow[0].EXISTFLAG === 'EXIST') {
-            console.log('가입된 회원입니다.');
-            //토큰 발급
-            jwtToken = jwt.sign({
-              id: id,
-              email: email,
+          if (EXISTFLAG !== 'EXIST') {
+            let { jwtToken: access, refreshToken: refresh } = await signUp({
+              id,
+              email: '',
+              social: 'kakao',
+              isExist: true,
             });
-            refreshToken = jwt.refresh();
+            jwtToken = access;
+            refreshToken = refresh;
+          } else if (EXISTFLAG === 'EXIST') {
+            let { jwtToken: access, refreshToken: refresh } = await signUp({
+              id,
+              email: '',
+              social: 'kakao',
+              isExist: true,
+            });
+            jwtToken = access;
+            refreshToken = refresh;
           }
-
           // 발급한 refresh token을 redis에 key를 user의 id로 하여 저장합니다.
           //await redisClient.set(id, refreshToken);
-          // res.setHeader(
-          //   'Set-Cookie',
-          //   `access_token=${jwtToken}; path=/; expires=${new Date(
-          //     Date.now() + 60 * 60 * 24 * 1000 * 3, //3일
-          //   )}; httponly`,
-          // );
-          res.cookie('refresh-token', refreshToken, {
-            maxAge: 1000 * 60 * 60 * 24 * 1,
-            httpOnly: true,
-          });
-
-          const responseData = {
-            success: true,
-            userInfo,
-            accessToken: jwtToken,
-          };
-          return res.status(200).json(responseData);
+          return returnResponse({ res, jwtToken, refreshToken });
+        }
+      }
+      if (coperation === 'google') {
+        const { id, email, verified_email, picture } = userInfo;
+        if (id && email) {
+          //1. 회원가입 여부 확인
+          const memberRow = await authDaoNew.getEmailIsAlreadyExist(email);
+          const { EXISTFLAG } = memberRow[0];
+          let jwtToken = null;
+          let refreshToken = null;
+          //회원 가입 안되어있을시
+          if (EXISTFLAG !== 'EXIST') {
+            let { jwtToken: access, refreshToken: refresh } = await signUp({
+              id,
+              email,
+              social: 'google',
+              isExist: true,
+            });
+            jwtToken = access;
+            refreshToken = refresh;
+          } else if (EXISTFLAG === 'EXIST') {
+            let { jwtToken: access, refreshToken: refresh } = await signUp({
+              id,
+              email,
+              social: 'google',
+              isExist: false,
+            });
+            jwtToken = access;
+            refreshToken = refresh;
+          }
+          // 발급한 refresh token을 redis에 key를 user의 id로 하여 저장합니다.
+          //await redisClient.set(id, refreshToken);
+          return returnResponse({ res, jwtToken, refreshToken });
         }
       }
     }
   } catch (e) {
     console.log(e);
-  }
-});
-
-/* 로그인  */
-router.post('/login', async (req, res) => {
-  try {
-    let filter = {
-      MEM_EMAIL: req.body.memEmail,
-    };
-    let memberRow = await Member.find(filter);
-    if (memberRow && memberRow.length !== 0) {
-      console.log('isExist');
-      //온경우
-      const password = req.body.memPassword;
-      let jwtToken = await bcryptCheck(password, memberRow);
-      console.log(jwtToken);
-      // res.cookie('access-token', jwtToken, {
-      //   maxAge: 1000 * 60 * 60 * 24 * 1,
-      //   httpOnly: false
-      // });
-      if (jwtToken) {
-        return res.json({
-          message: 'logged in successfully',
-          token: jwtToken,
-          status: 200,
-        });
-      } else {
-        return res.json({ message: 'error', status: 400 });
-      }
-    } else {
-      return res.json({ message: '가입된 정보가 없습니다.', status: 400 });
-    }
-  } catch (e) {
-    console.log('error', e);
-    return res.json({ message: 'error', status: 400 });
   }
 });
 
@@ -316,75 +329,17 @@ checkValidationPassword = (password, res) => {
 router.get('/refresh', refresh);
 
 router.get('/test', async (req, res) => {
-  // res.setHeader(
-  //   'Set-Cookie',
-  //   `access_token=dsadasd; path=/; expires=${new Date(
-  //     Date.now() + 60 * 60 * 24 * 1000 * 3, //3일
-  //   )}; httponly`,
-  // );
-
-  // res.cookie('hello', 'value', {
-  //   maxAge: 10000,
-  // });
   res.cookie('access-tokens', 'abc', {
     maxAge: 1000 * 60 * 60 * 24 * 1,
     httpOnly: true,
   });
   res.send('hello updated.');
 });
+
 /* 
   profile
   middleware 적용 
 */
 router.get('/profile', authJwt, async (req, res) => {});
-
-/* local 회원가입 */
-router.post('/setMemberSignup', async (req, res) => {
-  console.log('hello setMemberSignUp');
-  //test
-  if (!checkValidationPassword(req.body.memPassword)) {
-    console.log('notVaildation');
-    return res.json({
-      message: 'fail',
-      code: 500,
-      //error: error
-    });
-  }
-  try {
-    let filter = {
-      MEM_EMAIL: req.body.memEmail,
-    };
-    let memberRow = await Member.find(filter);
-    if (memberRow && memberRow.length !== 0) {
-      return res.json({
-        status: 404,
-        message: 'email already exist',
-      });
-    } else {
-      let password = req.body.memPassword;
-      const bcySalt = await helper.getBcryptSalt();
-      const hashedPassword = await helper.getHashedPassword(password, bcySalt);
-      let userData = {
-        MEM_EMAIL: req.body.memEmail,
-        MEM_PASSWORD: hashedPassword,
-        MEM_USER_NAME: req.body.memUserName,
-        MEM_SIGN_TYPE: 'LOCAL',
-      };
-      const member = new Member(userData);
-      await member.save();
-      return res.json({
-        status: '200',
-        message: 'success sign up',
-      });
-    }
-  } catch (error) {
-    console.error(error);
-    return res.json({
-      message: 'fail',
-      code: 500,
-      error: error,
-    });
-  }
-});
 
 module.exports = router;
